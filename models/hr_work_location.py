0fbb61e1a0431ceb31cbe61fcfaea2761e393675
arash <homayounfar.arash@gmail.com>
2025-03-23 16:50:43 +0330

    uuid added

--- models/hr_work_location.py
+++ models/hr_work_location.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api, _
 class SdProjectsHrWorkLocation(models.Model):
     _inherit = "hr.work.location"
 
-    project_name = fields.Many2one('sd_projects.projects')
+    project_name = fields.Many2one('sd_projects.projects', default=lambda self: self.env.context.get('project_name'))
--- models/models.py
+++ models/models.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
-
+import uuid
+from icecream import ic
 
 class SdProjectsProjects(models.Model):
     _name = 'sd_projects.projects'
     _description = 'Projects'
+    _inherit = ['mail.thread']
 
-    name = fields.Char(required=True, translate=True)
-    project_no = fields.Char()
-    client = fields.Many2one('res.partner')
-    start_date = fields.Date()
+    name = fields.Char(required=True, translate=True, tracking=True)
+    project_no = fields.Char(tracking=True)
+    client = fields.Many2one('res.partner',tracking=True)
+    start_date = fields.Date(tracking=True)
+    uu_id = fields.Char(default= lambda self: uuid.uuid4().hex)
+
+
+    def update_uuid(self):
+        active_ids = self.env.context.get('active_ids')
+        records = self.browse(active_ids)
+        for rec in records:
+            rec.uu_id = uuid.uuid4().hex
