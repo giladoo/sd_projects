bf8381113a426107bcbfddb5ee77de55aee8ef0a
homayounfar <homayounfar@msn.com>
2025-07-29 09:19:24 +0330

    update

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -6,3 +6,9 @@ class SdProjectsHrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     project_name = fields.Many2one('sd_projects.projects')
+
+
+class SdProjectsHrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    project_name = fields.Many2one('sd_projects.projects')
